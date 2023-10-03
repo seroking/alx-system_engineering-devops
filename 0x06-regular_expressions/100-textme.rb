6071a736ccda9ab96c4a442a88f614f4e0265dbc
seroking <baridmohamedamine004@gmail.com>
2023-10-03 12:27:02 -0700

    done

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # matching cases
 
-puts ARGV[0].scan(/\[from:.+\] \[to:\+\d{11}\] \[flags:\d:\d:\d:\d:\d\]/).join
+puts ARGV[0].scan(/\[from:\+?\d{11}] \[to:\+?\d{11}\] \[flags:\d:\d:\d:\d:\d\]/).join
